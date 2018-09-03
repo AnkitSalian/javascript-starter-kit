3c5d1a64621ce9e791584d8a059740c21dc798d6
Ankit <salian.ankit@tcs.com>
2018-09-03 20:11:37 +0530

    Usage of json faker to create a dummy schema

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+    res.json([
+        {"id":1, "firstName": "Ankit", "lastName":"Salian", "email": "ankit@goole.com"},
+        {"id":2, "firstName": "Anit", "lastName":"Salian", "email": "anit@goole.com"},
+        {"id":3, "firstName": "Ashok", "lastName":"Salian", "email": "ashok@goole.com"}
+    ]);
+});
+
 app.listen(port, function(err){
     if(err){
         console.log(err);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-import './index.css';
-import numeral from 'numeral';
+import {getUsers, deleteUser} from './api/userApi';
+
+//Populate table of users via API call
 /* eslint-disable no-console */
-const courseValue = numeral(1000).format('$0,0.00');
-//debugger;
-console.log(`I would like to pay ${courseValue} for this awesome course!`);
+getUsers().then(result => {
+    let userBody = "";
+
+    result.forEach(user => {
+        console.log(user);//
+        userBody += `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+        </tr>`;
+    });
+
+    global.document.getElementById('users').innerHTML = userBody;
+
+    const deleteLinks = global.document.getElementByClassName('deleteUser');
+
+    Array.from(deleteLinks, link => {
+        link.onclick = function(event){
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attribute["data-id"].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
+});
